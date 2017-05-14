5780d5abe45db0ccc01c3f920325ad76d3b97824
sk-day <haridwiharyono@gmail.com>
2017-05-14 21:53:34 +0700

    first commit

---
+++ contras.py
@@ -0,0 +1,29 @@
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import misc
+
+def showImage(image, imageN):
+    plt.figure()
+    plt.title('Before')
+    plt.imshow(image, cmap=plt.cm.gray)
+    plt.figure()
+    plt.title('After')
+    plt.imshow(imageN, cmap=plt.cm.gray)
+    plt.show()
+
+def normalisasi(image):
+    h = np.histogram(image, bins=np.arange(257))[0]
+    H = np.cumsum(h)
+    H_m = np.ma.masked_equal(H,0)
+    H_m = (120/float((H_m.max()-H_m.min())/float(2)))*H_m
+    h = np.ma.filled(H_m,0).astype('uint8')
+    img_m = h[image]
+    return img_m
+
+def main():
+    img = misc.imread(sys.argv[1])
+    showImage(img ,normalisasi(img))
+
+if __name__ == "__main__":
+    main()
